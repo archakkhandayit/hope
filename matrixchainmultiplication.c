53334c9fe188291965dda44396b8ee864ad840b8
Archak Khandayit <168993192+archakkhandayit@users.noreply.github.com>
2025-05-05 13:03:47 +0530

    Create matrixchainmultiplication.c

---
+++ matrixchainmultiplication.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <limits.h>
+
+int matrixChainOrder(int p[], int n) {
+    int m[n][n];
+
+    for (int i = 1; i < n; i++)
+        m[i][i] = 0;
+
+    for (int L = 2; L < n; L++) {
+        for (int i = 1; i < n - L + 1; i++) {
+            int j = i + L - 1;
+            m[i][j] = INT_MAX;
+            for (int k = i; k < j; k++) {
+                int q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+                if (q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+    }
+
+    return m[1][n-1];
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of matrices: ");
+    scanf("%d", &n);
+
+    int p[n + 1];
+    printf("Enter dimensions (length %d): ", n + 1);
+    for (int i = 0; i <= n; i++) {
+        scanf("%d", &p[i]);
+    }
+
+    int result = matrixChainOrder(p, n + 1);
+    printf("Minimum number of multiplications is %d\n", result);
+
+    return 0;
+}
+
+
+
+
+Enter number of matrices: 3
+Enter dimensions (length 4): 10 50 5 25
